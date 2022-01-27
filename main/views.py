42d2d2858f64533ad52f04af4c3c5d773b8e86fc
stayger <stayger@gmail.com>
2022-01-27 16:10:06 +0500

    5

--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from django.template import context
 from .models import Conc
 
 
+n = range(1, 13)
 def index(request):
     return render(request, 'main/index.html')
 
@@ -13,7 +14,8 @@ def about(request):
     return render(request, 'main/about.html')
 
 def add(request):
-    return render(request, 'main/add.html')
+    context={'n': range(1,13)}
+    return render(request, 'main/add.html',context=context)
 
 
 def add_conc(request):
@@ -21,7 +23,7 @@ def add_conc(request):
     data = json.loads(request.POST["data1"])
     for item in data:
         print(item)
-    
+
     context = {'month': month, 'data':data}
     return render(request, 'main/add_conc.html',context=context)
 
@@ -30,9 +32,6 @@ def report(request):
     month = '1'
     if request.method == 'POST':
         month = request.POST['month']
-
-
-
     items = Conc.objects.filter(dt__month=month) \
         .aggregate(Min('fe'), Max('fe'), Avg('fe'),
                    Min('si'), Max('si'), Avg('si'),
@@ -40,5 +39,6 @@ def report(request):
                    Min('ca'), Max('ca'), Avg('ca'),
                    Min('s'), Max('s'), Avg('s'),
                    )
-    months = range(1, 13)
-    return render(request, 'main/report.html', {'items': items, 'months': months, 'mm': month})
+    
+    context={'items': items, 'n': range(1,13)}
+    return render(request, 'main/report.html', context=context)
